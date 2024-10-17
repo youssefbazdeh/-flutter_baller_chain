4f754dfbed33d4145fe32b4a491ae197f7fb4570
youssefbazdeh <75418367+youssefbazdeh@users.noreply.github.com>
2024-10-17 14:15:39 +0100

    Delete lib directory

--- lib/utils/const.dart
+++
@@ -1,9 +0,0 @@
-
-
-
-
-final base_url="http://172.20.10.4:9090";
-final image_url="https://www.flashscore.com/";
-//final image="image"
-//172.25.64.1
-//192.168.1.144
--- lib/utils/routes.dart
+++
@@ -1,5 +0,0 @@
-const splashScreen = '/SplashScreen';
-const loginRoute = '/LoginPage';
-const signUpRoute = '/RegistrationPage';
-const forgetPasswordRoute = '/ForgotPasswordPage';
-const profileRoute ='/ProfileView';
